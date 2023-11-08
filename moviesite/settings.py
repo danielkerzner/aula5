594403982a61d5c9e29bae0bba62769e5a4e7377
Daniel Kerzner <daniel.kerzner@usp.br>
2023-11-08 14:38:19 -0300

    Implementar autenticação, com cadastro e login de usuários

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'staticpages.apps.StaticpagesConfig',
     'movies.apps.MoviesConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
